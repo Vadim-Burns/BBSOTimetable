398c06efdcf003b5289d00327ab0c31cadf7c5ac
Vadim-Burns <vadim.burns@protonmail.com>
2022-02-28 11:58:58 +0000

    keyboard

---
+++ src/keyboards.py
@@ -0,0 +1,11 @@
+from telebot import types
+
+main_markup = types.ReplyKeyboardMarkup()
+current_day = types.KeyboardButton('Расписание на сегодня')
+tomorrow_day = types.KeyboardButton('Расписание на завтра')
+after_tomorrow_day = types.KeyboardButton('Расписание на послезавтра')
+week = types.KeyboardButton('Расписание на текущую неделю')
+main_markup.row(current_day)
+main_markup.row(tomorrow_day)
+main_markup.row(after_tomorrow_day)
+main_markup.row(week)
--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
-print("start")
-print("F")
+import telebot
+from keyboards import main_markup
+
+bot = telebot.TeleBot("5187039085:AAFJfEjUNtG_ISbUS-LqE1-fRT1uAeAUrpo", parse_mode=None)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Hello there?", reply_markup=main_markup)
+
+
+if __name__ == '__main__':
+    bot.infinity_polling()
